be28c561a6cc9f35fa879bcfcc380cb8a894b95a
Rark7040 <crimera.hello@gmail.com>
2022-11-26 00:36:47 +0900

    wip in log

--- utils/log/logger.go
+++ utils/log/logger.go
@@ -1,15 +1,51 @@
 package log
 
+import "fmt"
+
 type Logger struct {
-	update   bool
 	ids      int
 	messages map[int]Message
 }
 
 func NewLogger() Logger {
 	return Logger{
-		update:   false,
 		ids:      0,
 		messages: map[int]Message{},
 	}
+}
+
+func (l *Logger) NeedsUpdate(p int) bool {
+	return p < l.ids
+}
+
+func (l *Logger) Logging(m Message) {
+	l.messages[l.ids] = m
+	l.ids += 1
+}
+
+func (l *Logger) GetTitle(id int) string {
+	if m, ok := l.messages[id]; ok {
+		return m.title
+	}
+	return ""
+}
+
+func (l *Logger) GetContent(id int) string {
+	if m, ok := l.messages[id]; ok {
+		return m.content
+	}
+	return ""
+}
+
+func (l *Logger) Clear() {
+	l.ids = 0
+	l.messages = map[int]Message{}
+}
+
+func (l *Logger) GetAll() *map[int]Message {
+	return &l.messages
+}
+
+func (l *Logger) Debug() {
+	fmt.Println(l.messages)
 }
