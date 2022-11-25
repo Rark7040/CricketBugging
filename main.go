24b282131da68d63967e34f04395f88ae6e4c279
Rark7040 <crimera.hello@gmail.com>
2022-11-26 00:23:02 +0900

    英語よわよわ

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ var assets embed.FS
 
 func main() {
 	app := NewApp()
+	binds := NewBinds() //binds backend funcs to frontend.
 	err := wails.Run(&options.App{
 		Title:  "CricketBugging",
 		Width:  1024,
@@ -23,7 +24,7 @@ func main() {
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
-			app,
+			binds,
 		},
 	})
 
