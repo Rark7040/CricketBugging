7637698648440be9c1c60a498947aca57089e2a5
Rark7040 <crimera.hello@gmail.com>
2022-11-25 23:53:20 +0900

    addr

---
+++ gophertunnel/address_info.go
@@ -0,0 +1,21 @@
+package gophertunnel
+
+type AddressInfo struct {
+	localIp  string
+	remoteIp string
+}
+
+func NewAddressInfo(local string, remote string) *AddressInfo {
+	return &AddressInfo{
+		localIp:  local,
+		remoteIp: remote,
+	}
+}
+
+func (addr *AddressInfo) GetLocalIp() string {
+	return addr.localIp
+}
+
+func (addr *AddressInfo) GetRemoteIp() string {
+	return addr.remoteIp
+}
