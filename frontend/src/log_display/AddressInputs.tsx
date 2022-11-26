e98c9c92f4955571bb4bde801dd59c26bdf911c6
Rark7040 <crimera.hello@gmail.com>
2022-11-26 16:04:14 +0900

    input

--- frontend/src/log_display/AddressInputs.tsx
+++ frontend/src/log_display/AddressInputs.tsx
@@ -1,6 +1,19 @@
+import {SyntheticEvent, useState} from "react";
+
 export function AddressesInputs() {
+    const [local, setLocal] = useState("0.0.0.0:19132");
+    const [remote, setRemote] = useState("s.mclife.pro:51091");
+    // @ts-ignore
+    const updateLocal = (event: SyntheticEvent) => setLocal(event.target.value);
+    // @ts-ignore
+    const updateRemote = (event: SyntheticEvent) => setRemote(event.target.value);
+
     return (
         <div id="AddressesInputs">
+            <div id="input" className="inputs-box">
+                <input id="local" className="input" onChange={updateLocal} autoComplete="off" name="input" value={local} type="text"/>
+                <input id="remote" className="input" onChange={updateRemote} autoComplete="off" name="input" value={remote} type="text"/>
+            </div>
         </div>
     );
 }
