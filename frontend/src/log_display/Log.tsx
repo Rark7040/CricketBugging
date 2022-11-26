1a8135a52be1156d5b91126600c9770708ae3fd6
Rark7040 <crimera.hello@gmail.com>
2022-11-26 18:37:04 +0900

    fix bug

--- frontend/src/log_display/Log.tsx
+++ frontend/src/log_display/Log.tsx
@@ -1,28 +1,32 @@
-import React, {Component, useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {GetLatestId, NeedsUpdate} from "../../wailsjs/go/main/WailsBinds";
 import {LogMessage} from "./LogMessage";
 import {createRoot} from "react-dom/client";
 import {SyntaxHighlight} from "./SyntaxHightLight";
 import Prism from "prismjs";
 import {AutoScrollAtom} from "./atom/AutoScrollAtom";
-import {useRecoilValue} from "recoil";
+import {useRecoilState, useRecoilValue} from "recoil";
 import {ScrollLog} from "./function/ScrollLog";
+import {LogAtom} from "./atom/LogAtom";
 
-export class Log extends Component {
-    private static ids: number = 0;
-    private static interval: number|undefined = undefined;
+export function Log () {
+    const [log, setLog] = useRecoilState(LogAtom);
+    const auto_scroll = useRecoilValue(AutoScrollAtom);
 
-    private checkUpdate(logs: LogMessage[], setLog: Function) {
-        NeedsUpdate(Log.ids).then((needsUpdate: boolean) => {
+    let ids: number = 0;
+    let interval: number|undefined = undefined;
+
+    function checkUpdate(logs: LogMessage[], setLog: Function) {
+        NeedsUpdate(ids).then((needsUpdate: boolean) => {
             if(!needsUpdate) return;
             GetLatestId().then((latestId: number) => {
-                Log.getLogsBetween(Log.ids, latestId).then((newLogs) => setLog(logs.concat(newLogs)));
-                Log.ids = latestId; //sync to backend logger id
+                getLogsBetween(ids, latestId).then((newLogs) => setLog(logs.concat(newLogs)));
+                ids = latestId; //sync to backend logger id
             });
         })
     }
 
-    private static getLogsBetween(startId: number, endId: number): Promise<Awaited<LogMessage>[]> {
+    function getLogsBetween(startId: number, endId: number): Promise<Awaited<LogMessage>[]> {
         let promises: Promise<LogMessage>[] = [];
 
         for (let id = startId; id <= endId; ++id){
@@ -31,7 +35,7 @@ export class Log extends Component {
         return Promise.all(promises);
     }
 
-    private static createLogContents(logs: LogMessage[]) {
+    function createLogContents(logs: LogMessage[]) {
         let log_elements: React.DetailedReactHTMLElement<any, HTMLElement>[] = [];
         logs.forEach((log: LogMessage) => log_elements.push(log.toElement()));
 
@@ -43,26 +47,22 @@ export class Log extends Component {
         }));
     }
 
-    public render(){
-        const [log, setLog] = useState([]);
-        const auto_scroll = useRecoilValue(AutoScrollAtom);
-        Log.interval ??= setInterval( () => this.checkUpdate(log, setLog), 20);
-        Log.createLogContents(log);
+    interval ??= setInterval( () => checkUpdate(log, setLog), 10000000000);
+    createLogContents(log);
 
-        useEffect(() => {
-            Prism.highlightAll();
+    useEffect(() => {
+        Prism.highlightAll();
 
-            if(auto_scroll){
-                ScrollLog()
-            }
-        }, [log]);
+        if(auto_scroll){
+            ScrollLog()
+        }
+    }, [log]);
 
-        return  (
-            <div id="Log">
-                <SyntaxHighlight>
-                    <div className="log-container" id="log-container"/>
-                </SyntaxHighlight>
-            </div>
-        );
-    }
+    return  (
+        <div id="Log">
+            <SyntaxHighlight>
+                <div className="log-container" id="log-container"/>
+            </SyntaxHighlight>
+        </div>
+    );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,11 @@ import {createRoot} from 'react-dom/client'
 import './css/style.css'
 import App from './App'
 
-const container = document.getElementById('root')
-
-const root = createRoot(container!)
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
 root.render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>
-)
+);
